eab79fda33f053c11b758041ca51fdc4b323e685
Olli Kiikkilä <kiikkila.olli@gmail.com>
2021-05-15 22:43:20 +0300

    Backend initial setup

--- back/index.ts
+++ back/index.ts
@@ -1,11 +1,88 @@
 import express from 'express';
-// rest of the code remains same
+import http from 'http';
+import { Server } from "socket.io";
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const server: http.Server = http.createServer(app);
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+const io: Server = require("socket.io")(server, {
+  cors: {
+    origin: "*:*",
+    methods: ["GET", "POST"],
+    credentials: false
+  }
+});
 
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+const rooms: Record<string, { message: string, timestamp: string }> = {}
+
+let connections = 0;
+const addConnections = () => connections += 1;
+const removeConnections = () => connections -= 1;
+
+io.on('connection', (socket) => {
+  if (!socket.handshake.headers.origin) {
+    // high chance it's a script kiddie
+    socket.disconnect();
+    return;
+  }
+
+  addConnections();
+  console.info(`Socket count: ${connections}`);
+
+  socket.on('newConnection', (roomId: string) => {
+    if (roomId in rooms) {
+      socket.join(roomId);
+      socket.emit('message', rooms[roomId])
+    } else {
+      socket.emit('error', "No room found");
+      socket.disconnect();
+    }
+  })
+  
+  socket.on('newMessage', ({ roomId, message }) => {
+    if (roomId in rooms) {
+      // Update latest message
+      rooms[roomId] = { message, timestamp: new Date().toISOString() };
+      io.to(roomId).emit('message', message)
+    } else {
+      socket.emit('error', "No room found");
+      socket.disconnect();
+    }
+  })
+
+  socket.once('disconnect', () => {
+    removeConnections();
+  });
+});
+
+setInterval(() => {
+  Object.entries(rooms).forEach(([key, value]) => {
+    const date = new Date(value.timestamp);
+    
+    // Delete rooms that have been idle for over 3 days
+    if (Date.now() - date.getTime() > 1000 * 60 * 60 * 24 * 3) {
+      delete rooms[key];
+    }
+  });
+})
+
+const PORT = Number(process.env.PORT) || 8000;
+
+io.listen(PORT);
+
+// ROUTES
+  
+app.get('/', (req, res) => res.send('Hello world!'));
+
+app.post('/create', (req, res) => {
+  const roomId = req.body.roomId || uuidv4();
+  rooms[roomId] = { message: "Hello", timestamp: new Date().toISOString() };
+
+  res.redirect(`/admin/${roomId}`)
+});
+
+app.listen(PORT + 1, () => {
+  console.info(`WebSocket is listening on port ${PORT}!`);
+  console.info(`App is listening on port ${PORT + 1}!`);
 });
