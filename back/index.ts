47940ebc8d8a2c12a85dcb454c2205b2da9da7bc
Olli Kiikkilä <kiikkila.olli@gmail.com>
2021-05-15 23:08:52 +0300

    Eslint

--- back/index.ts
+++ back/index.ts
@@ -1,24 +1,24 @@
 import express from 'express';
 import http from 'http';
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
 import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const server: http.Server = http.createServer(app);
 
-const io: Server = require("socket.io")(server, {
+const io: Server = require('socket.io')(server, {
   cors: {
-    origin: "*:*",
-    methods: ["GET", "POST"],
-    credentials: false
-  }
+    origin: '*:*',
+    methods: ['GET', 'POST'],
+    credentials: false,
+  },
 });
 
-const rooms: Record<string, { message: string, timestamp: string }> = {}
+const rooms: Record<string, { message: string; timestamp: string }> = {};
 
 let connections = 0;
-const addConnections = () => connections += 1;
-const removeConnections = () => connections -= 1;
+const addConnections = () => (connections += 1);
+const removeConnections = () => (connections -= 1);
 
 io.on('connection', (socket) => {
   if (!socket.handshake.headers.origin) {
@@ -33,23 +33,23 @@ io.on('connection', (socket) => {
   socket.on('newConnection', (roomId: string) => {
     if (roomId in rooms) {
       socket.join(roomId);
-      socket.emit('message', rooms[roomId])
+      socket.emit('message', rooms[roomId]);
     } else {
-      socket.emit('error', "No room found");
+      socket.emit('error', 'No room found');
       socket.disconnect();
     }
-  })
-  
+  });
+
   socket.on('newMessage', ({ roomId, message }) => {
     if (roomId in rooms) {
       // Update latest message
       rooms[roomId] = { message, timestamp: new Date().toISOString() };
-      io.to(roomId).emit('message', message)
+      io.to(roomId).emit('message', message);
     } else {
-      socket.emit('error', "No room found");
+      socket.emit('error', 'No room found');
       socket.disconnect();
     }
-  })
+  });
 
   socket.once('disconnect', () => {
     removeConnections();
@@ -59,27 +59,27 @@ io.on('connection', (socket) => {
 setInterval(() => {
   Object.entries(rooms).forEach(([key, value]) => {
     const date = new Date(value.timestamp);
-    
+
     // Delete rooms that have been idle for over 3 days
     if (Date.now() - date.getTime() > 1000 * 60 * 60 * 24 * 3) {
       delete rooms[key];
     }
   });
-})
+});
 
 const PORT = Number(process.env.PORT) || 8000;
 
 io.listen(PORT);
 
 // ROUTES
-  
+
 app.get('/', (req, res) => res.send('Hello world!'));
 
 app.post('/create', (req, res) => {
   const roomId = req.body.roomId || uuidv4();
-  rooms[roomId] = { message: "Hello", timestamp: new Date().toISOString() };
+  rooms[roomId] = { message: 'Hello', timestamp: new Date().toISOString() };
 
-  res.redirect(`/admin/${roomId}`)
+  res.redirect(`/admin/${roomId}`);
 });
 
 app.listen(PORT + 1, () => {
