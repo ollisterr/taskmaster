09985ddd8bfa10f3ff97a0e24a7a5933b269be47
Olli Kiikkilä <kiikkila.olli@gmail.com>
2021-05-22 23:38:07 +0300

    Fix backend nginx routing

--- front/src/utils/config.ts
+++ front/src/utils/config.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-export const API_URL = `${window.location.origin.match('localhost') ? 'http://localhost:8001' : `${window.location.origin}:4001`}`;
+export const API_URL = `${window.location.origin.match('localhost') ? 'http://localhost:8001' : `${window.location.origin}:8001`}`;
 
-export const SOCKET_URL = `${window.location.origin.match('localhost') ? 'http://localhost:8000' : `${window.location.origin}:4000`}`;
+export const SOCKET_URL = `${window.location.origin.match('localhost') ? 'http://localhost:8000' : `${window.location.origin}:8000`}`;
 
 export const socket = io(SOCKET_URL);
