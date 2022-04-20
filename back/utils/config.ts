73c19a464f80e1fa84c4420f379293935df43fe5
Olli Kiikkilä <kiikkila.olli@gmail.com>
2022-04-20 23:03:33 +0300

    Fix linting

--- back/utils/config.ts
+++ back/utils/config.ts
@@ -1,4 +1,6 @@
 import path from 'path';
 
 export const FILES_PATH =
-  process.env.NODE_ENV === 'production' ? '/app/files' : path.join(__dirname, '..', 'files');
+  process.env.NODE_ENV === 'production'
+    ? '/app/files'
+    : path.join(__dirname, '..', 'files');
